4ab4062c5eeeaf7a5809369c9284d74c62f15bf6
Brett Stevenson <bstevensondev@gmail.com>
2020-05-13 21:45:40 -0700

    Update site config

--- config/website.js
+++ config/website.js
@@ -1,17 +1,15 @@
-const tailwind = require('../tailwind')
-
 module.exports = {
   pathPrefix: `/`, // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: `Bob Ross`, // Navigation and Site Title
-  siteTitleAlt: `Bob Ross`, // Alternative Site title for SEO
-  siteTitleShort: `Bob Ross`, // short_name for manifest
-  siteHeadline: `Bob Ross`, // Headline for schema.org JSONLD
+  siteTitle: `The Joy of Testing`, // Navigation and Site Title
+  siteTitleAlt: `We don't make mistakes, we just have happy accidents.`, // Alternative Site title for SEO
+  siteTitleShort: `Joy of Testing`, // short_name for manifest
+  siteHeadline: `The Joy of Testing`, // Headline for schema.org JSONLD
   siteUrl: `https://joy-of-testing.netlify.com`, // Domain of your site. No trailing slash!
-  siteKeywords: [`bob-ross`, `paintings`, `disqus`],
+  siteKeywords: [`joy`, `of`, `testing`, `bob-ross`, `quotes`, `painting`, `disqus`, `lorem`, `ipsum`],
   siteLanguage: `en`, // Language Tag on <html> element
   siteLogo: `/icon.png`, // Used for SEO and manifest
-  siteDescription: `Bob Ross`,
+  siteDescription: `We don't make mistakes, we just have happy accidents.`,
   author: {
     name: `Brett Stevenson`,
     bio: `Software engineer at Disqus`,
