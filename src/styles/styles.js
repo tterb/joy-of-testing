3b426e570c6c4e1a3585e2fc40798d4bf239be4d
Brett Stevenson <bstevensondev@gmail.com>
2020-05-13 23:25:44 -0700

    Update stylesheets

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,5 +1,4 @@
 import { css } from 'styled-components'
-import theme from '../../config/theme'
 
 const styles = css`
   *,
@@ -7,25 +6,15 @@ const styles = css`
   *:after {
     box-sizing: border-box;
   }
-  html,
-  body {
-    margin: 0;
-    padding: 0;
-  }
   
   body {
-    font-family: 'Lato', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     font-size: 16px;
-    font-feature-settings: 'liga'0;
+    font-feature-settings: 'liga' 0;
     font-variant-ligatures: none;
     backface-visibility: hidden;
   }
 
   #___gatsby {
-    height: 100% !important;
-    > div {
-      height: 100% !important;
-    }
     .tl-edges {
       height: 100vh !important;
       .tl-wrapper {
