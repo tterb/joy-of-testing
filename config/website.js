d4fad81148109a3c100edf7319c37f8fdbc6b8a0
Brett Stevenson <bstevensondev@gmail.com>
2020-05-27 10:48:17 -0700

    Update site URL

--- config/website.js
+++ config/website.js
@@ -5,7 +5,7 @@ module.exports = {
   siteTitleAlt: `We don't make mistakes, we just have happy accidents.`, // Alternative Site title for SEO
   siteTitleShort: `Joy of Testing`, // short_name for manifest
   siteHeadline: `The Joy of Testing`, // Headline for schema.org JSONLD
-  siteUrl: `https://joy-of-testing.netlify.com`, // Domain of your site. No trailing slash!
+  siteUrl: `https://joyoftesting.com`, // Domain of your site. No trailing slash!
   siteKeywords: [`joy`, `of`, `testing`, `bob-ross`, `quotes`, `painting`, `disqus`, `lorem`, `ipsum`],
   siteLanguage: `en`, // Language Tag on <html> element
   siteLogo: `/icon.png`, // Used for SEO and manifest
