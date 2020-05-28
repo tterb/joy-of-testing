7f1849782f1753a1810f0e9b7741d841eb7311ce
Brett Stevenson <bstevensondev@gmail.com>
2020-05-27 23:50:24 -0700

    Improve dark/light theme switch styling

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -8,44 +8,37 @@ import { rgba } from 'polished'
 import { Moon, Sun } from '@styled-icons/boxicons-solid'
 
 const Wrapper = styled.div`
-	${tw`relative flex items-center justify-center`}
+	${tw`relative flex items-center justify-center w-15`}
 	top: 0;
 	left: 0.75rem;
-	width: 3.75rem;
 	height: 1.875rem;
 `
 
 const Base = styled.div`
-	${tw`absolute relative flex items-center justify-between rounded-full p-2 cursor-pointer z-0`}
-	background: ${props => `${rgba(props.color, 0.15)}`};
-	width: 3.75rem;
+	${tw`absolute relative flex items-center justify-between w-15 rounded-full p-2 cursor-pointer z-0`}
+	/* background: ${props => `${rgba(props.color, 0.15)}`}; */
+	background: ${props => `${props.color}`};
 	height: 1.875rem;
 	box-shadow: inset 0px -1px 6px -2px rgba(0,0,0,0.2);
 `
 
 const Switch = styled.span`
-	${tw`absolute rounded-full cursor-pointer z-10`}
+	${tw`absolute w-7 h-7 rounded-full cursor-pointer z-10`}
 	background: ${props => `${props.color}`};
-	width: 1.75rem;
-	height: 1.75rem;
 	left: 2px;
 	box-shadow: 1px 1px 8px -5px rgba(0,0,0,0.3);
 	transition: transform 0.2s linear;
 `
 
 const MoonIcon = styled(Moon)`
-	${tw`relative z-5`}
+	${tw`relative w-5 h-5 z-5`}
 	color: ${props => `${props.color}`};
-	width: 1.25rem;
-	height: 1.25rem;
 	left: -0.125rem;
 `
 
 const SunIcon = styled(Sun)`
-	${tw`relative z-5`}
+	${tw`relative w-5 h-5 z-5`}
 	color: ${props => `${props.color}`};
-	width: 1.25rem;
-	height: 1.25rem;
 	right: -0.125rem;
 `
 
@@ -62,7 +55,7 @@ class ThemeSwitcher extends React.Component {
 	}
 
 	componentDidUpdate() {
-		const xPos = (this.props.themeString === 'dark' ? 28 : 0)
+		const xPos = (this.props.themeString === 'dark' ? 29 : 0)
 		this.updateSwitchPosition(xPos)
 	}
 
