1d1939d05931c9fa358d6a6d75991e267d1f9825
Brett Stevenson <bstevensondev@gmail.com>
2021-04-06 11:13:45 -0700

    Fix SEO post config

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -21,7 +21,7 @@ const Head = props => {
 
   if (article) {
     const postMeta = node.frontmatter
-    const postImage = postMeta.cover.childImageSharp.resize.src
+    const postImage = postMeta.cover.childImageSharp.fluid.src
     title = `${postMeta.title} | ${site.siteTitle}`
     description = node.frontmatter.description
     image = `${homeURL}${postImage}`
